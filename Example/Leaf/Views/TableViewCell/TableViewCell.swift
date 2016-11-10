bda33fcc6a829095aac2468d1f7d491fd3c2784e
dstraughan <dstraughan@fuzzproductions.local>
2016-11-10 17:48:20 -0500

    [Initial Commit] built basic Demo app - organized Pod

---
+++ Example/Leaf/Views/TableViewCell/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Leaf
+//
+//  Created by Dylan Straughan on 11/9/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var intLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configure(with model: Model) {
+        intLabel.text = model.int != nil ? "\(model.int!)" : ""
+        backgroundColor = model.color ?? UIColor.white
+    }
+}
---
+++ Leaf/Classes/Extentions/Array+Extension.swift
@@ -0,0 +1,45 @@
+//
+//  Array+Extension.swift
+//  Leaf
+//
+//  Created by Dylan Straughan on 11/7/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    
+    func groupedBySection<T: Hashable>(
+        groupBy: (Element) -> (T)) -> [Int: [Element]] {
+        
+        var mappedItems = [Int: [Element]]()
+        
+        var index = -1
+        var mapped = [T: Int]()
+        
+        for item in self {
+            let groupingValue = groupBy(item)
+            
+            if let i = mapped[groupingValue] {
+                // Grouped value has been mapped. Add this item to the corresponding section .
+                var cpy = mappedItems[i]
+                cpy?.append(item)
+                mappedItems[i] = cpy
+            } else {
+                // An item with a new grouping value.
+                // Increment section index, add it to the new section, then mark the grouping value as mapped.
+                index += 1
+                mappedItems[index] = [item]
+                mapped[groupingValue] = index
+            }
+        }
+        
+        return mappedItems
+    }
+    
+    func asSingleSection() -> [Int: [Element]] {
+        return [0: self]
+    }
+}
+	
