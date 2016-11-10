ce06a7316841a9ec548ec7a4c314d1d4f7c297ae
dstraughan <dstraughan@fuzzproductions.local>
2016-11-10 17:51:42 -0500

    Initial commit - wired up basic demo app

--- Example/Leaf/CollectionViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CollectionViewController.swift
-//  Leaf
-//
-//  Created by Dylan Straughan on 11/9/16.
-//  Copyright © 2016 CocoaPods. All rights reserved.
-//
-
-import Foundation
---
+++ Example/Tests/Array+Extentions_Tests.swift
@@ -0,0 +1,35 @@
+//
+//  Array+Extentions_Tests.swift
+//  Leaf
+//
+//  Created by Dylan Straughan on 11/10/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class Array_Extentions_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+        
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
